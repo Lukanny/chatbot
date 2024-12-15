3f187a8c2b50e37645f75320efcc5d3859ab0e55
Vinicius Loureiro Cavalcante <vinicius.loureiro.cavalcante12@gmail.com>
2024-12-15 18:06:31 -0400

    user creator class and unit tests.

--- UserCreator.py
+++ UserCreator.py
@@ -3,13 +3,13 @@ from UserDatabase import UserDatabase
 class UserCreator:
 
     def __init__(self, user_db: UserDatabase):
-        self.user_database = user_db()
+        self.user_database = user_db
 
     def is_user_registered(self, cpf: str) -> bool:
-        return self.user_database.get_user(cpf)
+        return self.user_database.get_user(cpf) is not None
 
     def register_new_user(self, user_data: dict) -> dict:
         if self.is_user_registered(user_data["cpf"]):
             raise ValueError('User already registered')
         self.user_database.add_new_user(user_data)
-        return {"status": "sucess", "message": f"User {user_data["nome"]} with CPF: {user_data['cpf']} registered with sucess!"}
+        return {"status": "success", "message": f"User {user_data['name']} with CPF: {user_data['cpf']} registered with success!"}
--- tests/test_usercreator.py
+++ tests/test_usercreator.py
@@ -1,9 +1,37 @@
 import unittest
 
+from UserCreator import UserCreator
+from UserDatabase import UserDatabase
+
 class TestUserCreator(unittest.TestCase):
 
     def test_is_user_registered(self):
-        pass
+        user_db = UserDatabase()
+        creator = UserCreator(user_db)
+        user_not_registered = {"name": "João", "cpf": "12345678901"}
+
+        is_user_registered = creator.is_user_registered(user_not_registered["cpf"])
+
+        self.assertFalse(is_user_registered)
 
     def test_register_new_user(self):
-        pass
+        user_db = UserDatabase()
+        creator = UserCreator(user_db)
+        user_to_register = {"name": "João", "cpf": "12345678901"}
+
+        result = creator.register_new_user(user_to_register)
+
+        self.assertEqual(result["status"], "success")
+        self.assertTrue(creator.is_user_registered(user_to_register["cpf"]))
+
+
+    def test_register_new_user_fail(self):
+        user_db = UserDatabase()
+        creator = UserCreator(user_db)
+        user_to_register = {"name": "João", "cpf": "12345678901"}
+
+        creator.register_new_user(user_to_register)
+
+        with self.assertRaises(ValueError) as context:
+            creator.register_new_user(user_to_register)
+        self.assertEqual(str(context.exception), "User already registered")
